519d6583f5ad7743ff3a546c2bb60af28be9a38c
Manoj Menon <mhmenon910@gmail.com>
2018-06-21 20:53:47 +0530

    dict

---
+++ dict.py
@@ -0,0 +1,9 @@
+dict_sample = {
+    "one": 1,
+    "two": 2,
+    "three": 3
+}
+print(dict_sample["one"])
+print('********* all keys and values*************')
+for key,val in dict_sample.items():
+    print(f'{key} => {val}')
